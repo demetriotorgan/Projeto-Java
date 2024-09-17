2b0adfbd0a109b7ffab35b57b2f623f4e2e2dfe5
Demétrio Aquino Torgan <112517818+demetriotorgan@users.noreply.github.com>
2024-09-17 09:39:36 -0300

    Add files via upload

---
+++ Intro/Primeiro Programa em Java/Condicional.java
@@ -0,0 +1,14 @@
+public class Condicional {
+  public static void main(String[] args) {
+    int anoLancamento = 2021;
+    boolean incuidoPlano = true;
+    double notaFilme = 8.1;
+
+    if (anoLancamento > 2020) {
+      System.out.println("Lançamentos");
+    } else {
+      System.out.println("Filme Retro");
+    }
+
+  }
+}
---
+++ Intro/Primeiro Programa em Java/Main (1).java
@@ -0,0 +1,54 @@
+//Aula 1 e 2 
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Esse é o Screen Match");
+    System.out.println("Filme Top Gun Maverick");
+
+    int anoDeLancamento = 2020;
+    System.out.println(anoDeLancamento);
+    System.out.println("Ano de Lançamento: " + anoDeLancamento);
+
+    boolean incluidoNoPlano = true;
+    double nota = 8.1;
+
+    // Precedencia
+    double media = (9.8 + 6.3 + 8.0) / 3;
+    System.out.println(media);
+
+    String sinopse;
+    sinopse = "Filme de aventura";
+    System.out.println(sinopse);
+
+    // Conversao de tipos Casting
+    int classificacao;
+    classificacao = (int) media / 2;
+    System.out.println(classificacao);
+
+    // Crie um programa que realize a média de duas notas decimais e exiba o
+    // resultado.
+
+    // Declare uma variável do tipo double e uma variável do tipo int. Faça o
+    // casting da variável double para int e imprima o resultado.
+
+    // Declare uma variável do tipo char (letra) e uma variável do tipo String
+    // (palavra). Atribua valores a essas variáveis e concatene-as em uma mensagem.
+
+    // Declare uma variável do tipo double precoProduto e uma variável do tipo int
+    // (quantidade). Calcule o valor total multiplicando o preço do produto pela
+    // quantidade e apresente o resultado em uma mensagem.
+
+    // Declare uma variável do tipo double valorEmDolares. Atribua um valor em
+    // dólares a essa variável. Considere que o valor de 1 dólar é equivalente a
+    // 4.94 reais. Realize a conversão do valor em dólares para reais e imprima o
+    // resultado formatado.
+
+    // Declare uma variável do tipo double precoOriginal. Atribua um valor em reais
+    // a essa variável, representando o preço original de um produto. Em seguida,
+    // declare uma variável do tipo double percentualDesconto e atribua um valor
+    // percentual de desconto ao produto (por exemplo, 10 para 10%). Calcule o valor
+    // do desconto em reais, aplique-o ao preço original e imprima o novo preço com
+    // desconto.
+
+  }
+}
